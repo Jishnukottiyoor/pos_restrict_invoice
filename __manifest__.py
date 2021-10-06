6499b15898f68e1fdfa2ef92f015cfc8b6a40978
Jishnukottiyoor <jishnukkm009@gmail.com>
2021-10-06 22:15:56 +0530

    pos restrict invoice download

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'POS Restrict Invoice Download',
+    'version': '14.0.1.0.0',
+    'sequence': 10,
+    'summary': """POS Restrict Invoice Download""",
+    'description': """"This module helps you to Customize the POS and  Restrict invoice download""",
+    'category': 'Point Of Sale',
+    'author': 'sarath , jishnu',
+    'company': '',
+    'website': "",
+    'depends': ['base', 'point_of_sale'],
+    'data': ['views/pos_restrict_invoice.xml'],
+    'qweb': [],
+    'images': ['static/description/cancel.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
